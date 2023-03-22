326ec8ee6d5db8f785b4961baacce8a63df7ab74
Gustavo Batista <98623266+Guu-batista@users.noreply.github.com>
2023-03-22 11:50:11 -0300

    Commit checkpoint2

---
+++ CheckPoint2/checkpoint2.playground/Contents.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+let albums = ["Red", "Fearless", "Red", "1989", "Folklore", "Evermore"]
+
+let uniqueAlbums = Set(albums)
+
+print("Total albums: \(albums.count), unique albums: \(uniqueAlbums.count)")
